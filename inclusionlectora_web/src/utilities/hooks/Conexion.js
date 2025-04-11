c49d82720108112a11657ef87d6ad5b5face61df
yovin01 <stiven-yovin@hotmail.com>
2025-04-11 12:15:00 -0500

    Fix: update url of Conexion.js

--- inclusionlectora_web/src/utilities/hooks/Conexion.js
+++ inclusionlectora_web/src/utilities/hooks/Conexion.js
@@ -1,5 +1,5 @@
-const URL_BACKEND = "https://computacion.unl.edu.ec/lecyov:3007/api"
-export const URLBASE = "https://computacion.unl.edu.ec/lecyov:3007/"; 
+const URL_BACKEND = "https://computacion.unl.edu.ec/lecyov/api/api"
+export const URLBASE = "https://computacion.unl.edu.ec/lecyov/api"; 
 
 export const loginpost = async (formData, URL) => {
     const headers = {
